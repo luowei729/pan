546d99d3396c325e4f973994d86298482e4f7a69
luowei729 <1084605775@qq.com>
2026-03-27 00:30:43 +0800

    1

--- app/index.php
+++ app/index.php
@@ -59,7 +59,7 @@ $default_timezone = 'Asia/Shanghai'; // 北京时间
 
 // Root path for file manager
 // use absolute path of directory i.e: '/var/www/folder' or $_SERVER['DOCUMENT_ROOT'].'/folder'
-$root_path = './app';
+$root_path = './www';
 
 // Root url for links in file manager.Relative to $http_host. Variants: '', 'path/to/subfolder'
 // Will not working if $root_path will be outside of server document root
